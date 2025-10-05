287972b1ed876a837f4112cc5d8c6386ee66d568
Sushanth N <sushanthnandeti96@gmail.com>
2025-10-04 23:51:16 -0400

    Model inferenced

--- inference_classifier.py
+++ inference_classifier.py
@@ -1,5 +1,11 @@
 import cv2 
 import mediapipe as mp
+import pickle
+import numpy as np
+
+model_dict = pickle.load(open('./model.p', 'rb'))
+model = model_dict['model']
+
 
 cap = cv2.VideoCapture(0)
 
@@ -9,7 +15,10 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.3)
 
+labels_dict = { 0: 'A', 1: 'B', 2 : 'L'}
+
 while True: 
+    data_aux = []
     ret, frame = cap.read()
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -26,8 +35,23 @@ while True:
                 mp_drawing_styles.get_default_hand_connections_style()
             )        
 
+
+        for hand_landmarks in results.multi_hand_landmarks:
+             for i in range(len(hand_landmarks.landmark)):
+                x = hand_landmarks.landmark[i].x
+                y = hand_landmarks.landmark[i].y
+                data_aux.append(x)
+                data_aux.append(y)
+                    
+                
+        prediction = model.predict([np.asarray(data_aux)])
+        predicted_character = labels_dict[int(prediction[0])]
+
+        print(predicted_character)
+
+
     cv2.imshow('frame', frame)
-    cv2.waitKey(25)
+    cv2.waitKey(1)
 
 cap.release()
 cv2.destroyAllWindows()
